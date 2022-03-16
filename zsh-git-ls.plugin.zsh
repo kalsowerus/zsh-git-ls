2bb6759880725e6729153978ad752b9d50da81dd
kalsowerus <kalsowerus@protonmail.ch>
2022-03-16 19:42:25 +0100

    Fix git repository check

--- zsh-git-ls.plugin.zsh
+++ zsh-git-ls.plugin.zsh
@@ -46,7 +46,8 @@ function .zsh_git_ls_list_dir() {
     local list=$(command ls -l --color $ls_opts $dir)
 
     local git_status
-    if git_status=$(command git -C "$dir" status -s --ignored -unormal 2>/dev/null | command grep -v '/'); then
+    if git_status=$(command git -C "$dir" status -s --ignored -unormal 2>/dev/null); then
+        git_status=$(echo "$git_status" | grep -v '/')
         declare -A file_status
         for file in "${(f)git_status}"; do
             filename=$(echo "${file:3}" | sed -r 's/(.* -> )?(.*)/\2/g')
