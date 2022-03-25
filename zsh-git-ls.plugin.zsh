28573f9a1b138aeb850b076a708c290fd5f873e6
kalsowerus <kalsowerus@protonmail.ch>
2022-03-25 22:11:51 +0100

    Use stat for easier parsing

--- zsh-git-ls.plugin.zsh
+++ zsh-git-ls.plugin.zsh
@@ -1,41 +1,31 @@
 function git-ls() {
-    zparseopts -D -E -F -a ls_opts - \
-        a -all \
-        A -almost-all \
-        -author \
-        -block-size: \
-        B -ignore_backups \
-        c \
-        d -directory \
-        f \
-        F -classify \
-        -file-type \
-        -full-time \
-        g \
-        -group-directories-first \
-        G -no-group \
-        h -human_readable \
-        -si \
-        -hide: \
-        -indicator-style: \
-        i -inode \
-        I: -ignore: \
-        k -kibibytes \
-        n -numeric-uid-gid \
-        o \
-        p \
-        r -reverse \
-        R -recursive \
-        s -size \
-        S \
-        -sort: \
-        -time: \
-        -time-style: \
-        t \
-        u \
-        U \
-        v \
-        X \
+    zmodload zsh/datetime
+    local IFS='
+'
+    local DELIMITER='%'
+
+    zparseopts -D -E -F -a todo - \
+        a=ls_opts -all=ls_opts \
+        A=ls_opts -almost-all=ls_opts \
+        B=ls_opts -ignore_backups=ls_opts \
+        d=ls_opts -directory=ls_opts \
+        F=name_opts -classify=name_opts \
+        -file-type=name_opts \
+        -group-directories-first=ls_opts \
+        h=o_human_readable -human-readable=o_human_readable \
+        -si=o_si \
+        -hide:=ls_opts \
+        -indicator-style:=name_opts \
+        I:=ls_opts -ignore:=ls_opts \
+        p=name_opts \
+        r=ls_opts -reverse=ls_opts \
+        R=ls_opts -recursive=ls_opts \
+        S=ls_opts \
+        -sort:=ls_opts \
+        t=ls_opts \
+        U=ls_opts \
+        v=ls_opts \
+        X=ls_opts \
         -help=o_help 2>/dev/null
 
     if [[ $? != 0 ]]; then
@@ -49,95 +39,120 @@ function git-ls() {
     fi
 
     local dir=
-    local current_dir_status=
+    local git_status=
     local repo_path=
+    local stat=
+    local section=
+    local total=
     if (( $# < 2 )); then # no or one argument is given
         dir="${1:-.}"
-        current_dir_status="$(.zsh_git_ls_get_git_status "$dir")"
+        if [[ -d "$dir" ]]; then
+            total=0
+        fi
+        git_status="$(.zsh_git_ls_get_git_status "$dir")"
         repo_path="$(.zsh_git_ls_get_repo_path "$dir")"
     fi
 
     local list
-    list=$(command ls -l --quoting-style=shell --color=always $ls_opts $@)
+    list=$(command ls -N1 --color=never $ls_opts $@)
     local rc=$?
-    local section
 
-    for line in "${(@f)list}"; do
-        if [[ -z "$line" ]]; then # empty line separating sections when listing multiple files/directories
+
+    for filename in "${(@f)list}"; do
+        if [[ -z "$filename" ]]; then # empty line separating sections when listing multiple files/directories
+            .zsh_git_ls_print_section "$section" "$total"
+            section=
             dir=
-            current_dir_status=
+            git_status=
             repo_path=
+            total=0
             echo
-        elif [[ "$line" =~ '^(\S+):$' ]]; then # header line at the beginning of a directory list
+            continue
+        elif [[ "$filename" =~ '^(.+):$' ]]; then # header line at the beginning of a directory list
             dir="$match[1]"
-            current_dir_status=$(.zsh_git_ls_get_git_status "$dir")
+            git_status=$(.zsh_git_ls_get_git_status "$dir")
             repo_path=$(.zsh_git_ls_get_repo_path "$dir")
-            echo "$line"
-        elif [[ "$line" =~ '^total ' ]]; then # line showing total size, just echo
-            echo "$line"
+            echo "$filename"
+            continue
         elif [[ -z "$dir" ]]; then # $dir is not set: we are in the single file section
-            local filename=$(.zsh_git_ls_get_filename "$line")
-            local raw_filename=$(.zsh_git_ls_get_raw_filename "$filename")
-            dir=$(dirname "$raw_filename")
-            current_dir_status=$(.zsh_git_ls_get_git_status "$dir")
+            dir=$(dirname "$filename")
+            git_status=$(.zsh_git_ls_get_git_status "$dir")
             repo_path=$(.zsh_git_ls_get_repo_path "$dir")
-            .zsh_git_ls_parse_line "$line" "$filename" "$raw_filename" "$dir" "$current_dir_status" "$repo_path"
-            dir=
-        else # normal line in directory list
-            local filename=$(.zsh_git_ls_get_filename "$line")
-            local raw_filename=$(.zsh_git_ls_get_raw_filename "$filename")
-            .zsh_git_ls_parse_line "$line" "$filename" "$raw_filename" "$dir" "$current_dir_status" "$repo_path"
         fi
+
+        stat=($(command stat --printf '%A\n%h\n%G\n%U\n%s\n%Y\n%b\n%B\n%F' "$dir/$filename"))
+
+        if [[ -n "$total" ]]; then
+            (( total += $stat[7]*$stat[8] ))
+        fi
+
+        # basic information
+        section="$section$stat[1]$DELIMITER$stat[2]$DELIMITER$stat[3]$DELIMITER$stat[4]"
+
+        local size=
+        if [[ -n "$o_human_readable" ]]; then
+            size=$(numfmt --to=iec "$stat[5]")
+        elif [[ -n "$o_si" ]]; then
+            size=$(numfmt --to=si "$stat[5]")
+        else
+            size="$stat[5]"
+        fi
+        section="$section$DELIMITER$size"
+
+        section="$section$DELIMITER$(strftime '%b %e %H:%M' $stat[6])"
+
+        # git status character
+        local dir_path=$(realpath "$dir")
+        if [[ -n "$git_status" ]]; then
+            local path_prefix="${dir_path#$repo_path}"
+            local file_path="${path_prefix}/${filename}"
+            file_path="${file_path:1}"
+            local file_status=
+            if [[ -d "$repo_path/$file_path" ]]; then
+                local dir_status=$(echo "$git_status" | grep "^.. $file_path/")
+                if [[ "$dir_status" =~ '[ ?]. ' ]]; then # dirty
+                    file_status=' /'
+                elif [[ "$dir_status" =~ '.M ' ]]; then # modified & dirty
+                    file_status='/M'
+                elif [[ "$dir_status" =~ '.  ' ]]; then # modified
+                    file_status='/ '
+                elif .zsh_git_ls_is_ignored "$repo_path" "$file_path"; then
+                    file_status='!!'
+                fi
+            else
+                file_status="${$(echo "$git_status" | grep " $file_path$"):0:2}"
+                if [[ -z "$file_status" ]] && .zsh_git_ls_is_ignored "$repo_path" "$file_path"; then
+                    file_status='!!'
+                fi
+            fi
+            file_status_character=$(.zsh_git_ls_get_status_character "$file_status")
+        else
+            file_status_character=' '
+        fi
+        section="$section$DELIMITER$file_status_character"
+
+        # file name
+        local colored_filename=$(cd "$dir" && command ls --color=always -d "$filename")
+        section="$section$DELIMITER${colored_filename}"
+
+        section="$section\n"
     done
+
+    if [[ -n "$section" ]]; then
+        .zsh_git_ls_print_section "$section" "$total"
+    fi
 
     return $rc
 }
 
-function .zsh_git_ls_parse_line() {
-    local line="$1"
-    local filename="$2"
-    local raw_filename="$3"
-    local dir="$4"
-    local git_status="$5"
-    local repo_path="$6"
-    local file_status_character
+function .zsh_git_ls_print_section() {
+    local section="$1"
+    local total="$2"
 
-    if [[ "$git_status" != 'not_a_git_dir' ]]; then
-        local dir_path=$(realpath "$dir")
-        local path_prefix="${dir_path#$repo_path}"
-        local file_path="$path_prefix/${raw_filename:t}"
-        file_path="${file_path:1}"
-        local file_status
-        if [[ -d "$repo_path/$file_path" ]]; then
-            local dir_status=$(echo "$git_status" | grep "^.. $file_path/")
-            if [[ "$dir_status" =~ '[ ?]. ' ]]; then # dirty
-                file_status=' /'
-            elif [[ "$dir_status" =~ '.M ' ]]; then # modified & dirty
-                file_status='/M'
-            elif [[ "$dir_status" =~ '.  ' ]]; then # modified
-                file_status='/ '
-            elif .zsh_git_ls_is_ignored "$repo_path" "$file_path"; then
-                file_status='!!'
-            fi
-        else
-            file_status="${$(echo "$git_status" | grep " $file_path$"):0:2}"
-            if [[ -z "$file_status" ]] && .zsh_git_ls_is_ignored "$repo_path" "$file_path"; then
-                file_status='!!'
-            fi
-        fi
-        file_status_character=$(.zsh_git_ls_get_status_character "$file_status")
-    else
-        file_status_character=' '
+    if [[ -n "$total" ]]; then
+        echo "total $(numfmt --to=iec "$total")"
     fi
-    echo "${line%%$filename}$file_status_character $filename"
-}
-
-function .zsh_git_ls_get_git_status() {
-    if .zsh_git_ls_is_git_dir "$1"; then
-        echo "${$(command git -C "$1" status --porcelain -uall 2>/dev/null | grep -v '^!!' | sed 's/"//g'):-empty}"
-    else
-        echo 'not_a_git_dir'
-    fi
+    echo -n "$section" | column -t -s "$DELIMITER" | sed -r 's/ ( +)/\1/g' | sed -r 's/(\S+) ( +)/\2\1 /g'
 }
 
 function .zsh_git_ls_is_git_dir() {
@@ -146,14 +161,6 @@ function .zsh_git_ls_is_git_dir() {
 
 function .zsh_git_ls_get_repo_path() {
     command git -C "$1" rev-parse --show-toplevel 2>/dev/null
-}
-
-function .zsh_git_ls_get_filename() {
-    echo "$1" | perl -pe 's/^.*?\s((\x1B\[[0-9;]*m)?'\''.+->.+|(\x1B\[[0-9;]*m)?'\''.+|\s?\S+\s*->.+|\s?\S+)$/\1/'
-}
-
-function .zsh_git_ls_get_raw_filename() {
-    echo "$filename" |  sed 's/\x1B\[[0-9;]*m//g' | sed -r 's/^ ?'\''?([^'\'']+)'\''?.*$/\1/' | sed -r 's/^(.*?)\s+->\s+.*$/\1/g' | sed -r 's/^(.*?)[*/=>@|]$/\1/'
 }
 
 function .zsh_git_ls_is_ignored() {
@@ -186,7 +193,7 @@ function .zsh_git_ls_get_status_character() {
         echo -n "$DIRTY_COLOR$UNTRACKED_CHARACTER$RESET_COLOR"
         return
     elif [[ $1 == '!!' ]]; then # ignored
-        echo -n ' '
+        echo -n ' '
         return
     fi
 
@@ -226,36 +233,22 @@ Usage: $1 [OPTION]... [FILE]...
 For explanations for the following options see 'ls --help'.
     -a, --all
     -A, --almost-all
-        --author
-        --block-size=SIZE
     -B, --ignore-backups
-    -c
     -d, --directory
-    -f
     -F, --classify
         --file-type
-        --full-time
-    -g
         --group-directories-first
-    -G, --no-group
     -h, --human-readable
         --si
         --hide=PATTERN
         --indicator-style=WORD
-    -i, --inode
-    -k, --kibibytes
-    -n, --numeric-uid-gid
-    -o
+    -I, --ignore=PATTERN
     -p
     -r, --reverse
     -R, --recursive
-    -s, --size
     -S
         --sort=WORD
-        --time=WORD
-        --time-style=TIME_STYLE
     -t
-    -u
     -U
     -v
     -X
