ee9bee71639a4d7dd54fd122a7afabbe27a857e8
kalsowerus <kalsowerus@protonmail.ch>
2022-06-10 14:00:03 +0200

    Refactor to use globbing instead of calling ls

--- zsh-git-ls.plugin.zsh
+++ zsh-git-ls.plugin.zsh
@@ -1,34 +1,17 @@
 # shellcheck disable=SC2076
 
 function git-ls() {
+    setopt local_options warn_create_global
     zmodload zsh/datetime
     local IFS='
 '
-    local DELIMITER='\x00'
-
-    local ls_opts
     zparseopts -D -E -F -a todo - \
-        a=ls_opts -all=ls_opts \
-        A=ls_opts -almost-all=ls_opts \
-        B=ls_opts -ignore_backups=ls_opts \
-        d=ls_opts -directory=ls_opts \
-        F=name_opts -classify=name_opts \
-        -file-type=name_opts \
-        -group-directories-first=ls_opts \
+        a=o_all -all=o_all \
+        A=o_almost_all -almost-all=o_almost_all \
         h=o_human_readable -human-readable=o_human_readable \
         -si=o_si \
-        -hide:=ls_opts \
-        -indicator-style:=name_opts \
-        I:=ls_opts -ignore:=ls_opts \
-        p=name_opts \
-        r=ls_opts -reverse=ls_opts \
-        R=ls_opts -recursive=ls_opts \
-        S=ls_opts \
-        -sort:=ls_opts \
-        t=ls_opts \
-        U=ls_opts \
-        v=ls_opts \
-        X=ls_opts \
+        r=o_reverse -reverse=o_reverse \
+        S=o_S \
         -help=o_help 2>/dev/null
 
     # shellcheck disable=SC2181
@@ -42,130 +25,114 @@ function git-ls() {
         return
     fi
 
+    declare -a dirs
+    if (( $# < 2 )); then
+        dirs=("${1:-.}")
+    else
+        # shellcheck disable=SC2207,SC2296
+        dirs=($(echo "${(j:\n:)@}" | sort))
+    fi
+
     local dir=
-    local git_status=
-    local repo_path=
+    local files=
+    local file=
     local stat=
     local section=
+    local repo_path=
+    local size=
     local total=
-    if (( $# < 2 )); then # no or one argument is given
-        dir="${1:-.}"
-        if [[ -d "${dir}" ]]; then
-            total=0
-        fi
-        git_status="$(.zsh_git_ls_get_git_status "${dir}")"
-        repo_path="$(.zsh_git_ls_get_repo_path "${dir}")"
-    fi
-
-    local list
-    # shellcheck disable=SC2034,SC2086
-    list=$(command ls -N1 --color=never ${ls_opts} "$@")
-    local rc=$?
-
-
-    # shellcheck disable=SC2066,SC2296
-    local match
-    local dir_path
-    local colored_filename
-    for filename in "${(@f)list}"; do
-        if [[ -z "${filename}" ]]; then # empty line separating sections when listing multiple files/directories
-            .zsh_git_ls_print_section "${section}" "${total}"
-            section=
-            dir=
-            git_status=
-            repo_path=
-            total=0
-            echo
+    local dir_status
+    # shellcheck disable=SC2128,SC2250
+    for dir in $dirs; do
+        if [[ ! -d "${dir}" ]]; then
+            echo "cannot access '${dir}': Not a directory" >&2
             continue
-        elif [[ "${filename}" =~ ^(.+):$ ]]; then # header line at the beginning of a directory list
-            dir="${match[1]}"
-            git_status=$(.zsh_git_ls_get_git_status "${dir}")
-            repo_path=$(.zsh_git_ls_get_repo_path "${dir}")
-            echo "${filename}"
-            continue
-        elif [[ -z "${dir}" ]]; then # $dir is not set: we are in the single file section
-            dir=$(dirname "${filename}")
-            git_status=$(.zsh_git_ls_get_git_status "${dir}")
-            repo_path=$(.zsh_git_ls_get_repo_path "${dir}")
         fi
 
-        # shellcheck disable=SC2207
-        stat=($(command stat --printf '%A\n%h\n%G\n%U\n%s\n%Y\n%b\n%B\n%F' "${dir}/${filename}"))
+        section=
+        repo_path=
+        git_status=
+        total=0
+        if .zsh_git_ls_is_git_dir "${dir}"; then
+            repo_path=$(.zsh_git_ls_get_repo_path "${dir}")
+            git_status=$(.zsh_git_ls_get_git_status "${repo_path}")
+        fi
+        if (( $# > 1 )); then
+            echo "${dir}:"
+        fi
+        
+        files=()
+        if [[ -n "${o_almost_all}" ]]; then
+            setopt glob_dots
+        elif [[ -n "${o_all}" ]]; then
+            setopt glob_dots
+            files+=(. ..)
+        fi
+        files+=("${dir}"/*)
 
-        if [[ -n "${total}" ]]; then
+        # shellcheck disable=SC2128
+        for file in ${files}; do
+            # shellcheck disable=SC2207
+            stat=($(command stat --printf '%A\n%h\n%G\n%U\n%s\n%Y\n%b\n%B\n%F' "${file}"))
             (( total += stat[7]*stat[8] ))
-        fi
-
-        # basic information
-        section="${section}${stat[1]}${DELIMITER}${stat[2]}${DELIMITER}${stat[3]}${DELIMITER}${stat[4]}"
-
-        local size=
-        if [[ -n "${o_human_readable}" ]]; then
-            size=$(numfmt --to=iec "${stat[5]}")
-        elif [[ -n "${o_si}" ]]; then
-            size=$(numfmt --to=si "${stat[5]}")
-        else
-            size="${stat[5]}"
-        fi
-        section="${section}${DELIMITER}${size}"
-
-        section="${section}${DELIMITER}$(strftime '%b %e %H:%M' "${stat[6]}")"
-
-        # git status character
-        dir_path=$(realpath "${dir}")
-        if [[ -n "${git_status}" ]]; then
-            local path_prefix="${dir_path#"${repo_path}"}"
-            local file_path="${path_prefix}/${filename}"
-            file_path="${file_path:1}"
-            local file_status=
-            if [[ -d "${repo_path}/${file_path}" ]]; then
-                local dir_status
-                dir_status=$(echo "${git_status}" | grep "^.. ${file_path}/")
-                if [[ "${dir_status}" =~ '[ ?]. ' ]]; then # dirty
-                    file_status=' /'
-                elif [[ "${dir_status}" =~ '.M ' ]]; then # modified & dirty
-                    file_status='/M'
-                elif [[ "${dir_status}" =~ '.  ' ]]; then # modified
-                    file_status='/ '
-                elif .zsh_git_ls_is_ignored "${repo_path}" "${file_path}"; then
-                    file_status='!!'
-                fi
+            section="${section}${stat[1]}\t${stat[2]}\t${stat[3]}\t${stat[4]}"
+        
+            if [[ -n "${o_human_readable}" ]]; then
+                size=$(numfmt --to=iec "${stat[5]}")
+            elif [[ -n "${o_si}" ]]; then
+                size=$(numfmt --to=si "${stat[5]}")
             else
+                size="${stat[5]}"
+            fi
+            section="${section}\t${size}"
+        
+            section="${section}\t$(strftime '%b %e %H:%M' "${stat[6]}")"
+
+            # git status character
+            if [[ -n "${git_status}" ]]; then
                 # shellcheck disable=SC2300
-                file_status="${$(echo "${git_status}" | grep " ${file_path}$"):0:2}"
-                if [[ -z "${file_status}" ]] && .zsh_git_ls_is_ignored "${repo_path}" "${file_path}"; then
-                    file_status='!!'
+                file_path=${$(realpath "${file}")#${repo_path}/}
+                local file_status=
+                if [[ -d "${repo_path}/${file_path}" ]]; then
+                    dir_status=$(echo "${git_status}" | grep "^.. ${file_path}/")
+                    if [[ "${dir_status}" =~ '[ ?]. ' ]]; then # dirty
+                        file_status=' /'
+                    elif [[ "${dir_status}" =~ '.M ' ]]; then # modified & dirty
+                        file_status='/M'
+                    elif [[ "${dir_status}" =~ '.  ' ]]; then # modified
+                        file_status='/ '
+                    elif .zsh_git_ls_is_ignored "${repo_path}" "${file_path}"; then
+                        file_status='!!'
+                    fi
+                else
+                    # shellcheck disable=SC2300
+                    file_status="${$(echo "${git_status}" | grep " ${file_path}$"):0:2}"
+                    if [[ -z "${file_status}" ]] && .zsh_git_ls_is_ignored "${repo_path}" "${file_path}"; then
+                        file_status='!!'
+                    fi
                 fi
+                file_status_character=$(.zsh_git_ls_get_status_character "${file_status}")
+                section="${section}\t${file_status_character}"
             fi
-            file_status_character=$(.zsh_git_ls_get_status_character "${file_status}")
-        else
-            file_status_character=' '
+
+            # shellcheck disable=SC2164
+            section="${section}\t$(cd "$(dirname "$(realpath "${file}")")"; command ls -d --color=always "${file##*/}")\n"
+        done
+        echo "total $(numfmt --to=iec "${total}")"
+
+        # sorting
+        if [[ -n "${o_S}" ]]; then
+            section=$(echo "${section}" | sort -h -r -k 5)
         fi
-        section="${section}${DELIMITER}${file_status_character}"
-
-        # file name
-        colored_filename=$(cd "${dir}" && command ls --color=always -d "${filename}")
-        section="${section} ${colored_filename}"
-
-        local link=
-        link=$(readlink "${dir}/${filename}")
-        # shellcheck disable=SC2181
-        if [[ $? == 0 ]]; then
-            if [[ -e "${link}" ]]; then
-                section="${section} -> ${link}"
-            else
-                section="${section} -> \e[0;31m${link}\e[0m"
-            fi
+        if [[ -n "${o_reverse}" ]]; then
+            section=$(echo "${section}" | tac)
         fi
+        echo "${section}" | column -t -o ' ' -s $'\t'
 
-        section="${section}\n"
+        if (( $# > 1 )); then
+            echo
+        fi
     done
-
-    if [[ -n "${section}" ]]; then
-        .zsh_git_ls_print_section "${section}" "${total}"
-    fi
-
-    return "${rc}"
 }
 
 function .zsh_git_ls_get_git_status() {
@@ -183,44 +150,6 @@ function .zsh_git_ls_print_section() {
         echo "total $(numfmt --to=iec "${total}")"
     fi
     .zsh_git_ls_print_table "${section}"
-}
-
-function .zsh_git_ls_print_table() {
-    # shellcheck disable=SC2034
-    local content="${1}"
-    local widths=()
-
-    # shellcheck disable=SC2066,SC2296
-    for line in "${(@s/\n/)content}"; do
-        local i=1
-        # shellcheck disable=SC2066,SC2296
-        for field in "${(@s/\x00/)line}"; do
-            local length="${#field}"
-            if (( ${widths[i]:-0} < length )); then
-                widths[${i}]="${length}"
-            fi
-            (( i++ ))
-        done
-    done
-
-    # shellcheck disable=SC2066,SC2296
-    for line in "${(@s/\n/)content}"; do
-        if [[ -z "${line}" ]]; then
-            continue
-        fi
-        local i=1
-        # shellcheck disable=SC2298
-        for field in "${${(@s/\x00/)line}[@]:0:-1}"; do
-            if [[ "${field}" =~ '^[0-9]*.$' ]]; then
-                echo -n "${(l:${widths[${i}]}:: :)field} "
-            else
-                echo -n "${(r:${widths[${i}]}:: :)field} "
-            fi
-            (( i++ ))
-        done
-        # shellcheck disable=SC2298
-        echo "${${(@s/\x00/)line}[-1]}"
-    done
 }
 
 function .zsh_git_ls_is_git_dir() {
